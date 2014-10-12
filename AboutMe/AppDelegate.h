7ad3ee8bcc1548573c17fa5ec796a335b27aaab1
Rjervis3 <jervisre@grinnell.edu>
2014-10-12 09:18:58 -0500

    Initial Commit

---
+++ AboutMe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AboutMe
+//
+//  Created by Renn Jervis on 10/12/14.
+//  Copyright (c) 2014 Renn Jervis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AboutMe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AboutMe
+//
+//  Created by Renn Jervis on 10/12/14.
+//  Copyright (c) 2014 Renn Jervis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
